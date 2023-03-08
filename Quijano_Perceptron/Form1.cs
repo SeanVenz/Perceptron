e6a477fb1681313498ca1b98d9abe434df20ed74
Sean Venz Quijano <seanvenz.quijano@cit.edu>
2023-03-08 19:14:11 +0800

    Initial Commit

---
+++ Quijano_Perceptron/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Quijano_Perceptron
+{
+    public partial class Quijano_Perceptron : Form
+    {
+        Perceptron perceptron;
+        public Quijano_Perceptron()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int input1 = Convert.ToInt32(textBox1.Text);
+            int input2 = Convert.ToInt32(textBox2.Text);
+            int numOfEpochs = Convert.ToInt32(textBox3.Text);
+            // Example usage
+            perceptron = new Perceptron(numInputs: 2);
+            double[][] inputs = new double[][]
+            {
+                new double[] { 0, 0 },
+                new double[] { 0, 1 },
+                new double[] { 1, 0 },
+                new double[] { 1, 1 }
+            };
+            int[] targets = new int[] { 0, 1, 1, 1 };
+            perceptron.Train(inputs, targets, numOfEpochs);
+            string output = perceptron.Predict(new double[] { input1, input2 }).ToString(); // Output: 0
+            label1.Text = output;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Quijano_Perceptron/Perceptron.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quijano_Perceptron
+{
+    public class Perceptron
+    {
+        private double[] weights;
+        private double bias;
+        private double learningRate;
+
+        public Perceptron(int numInputs, double learningRate = 0.1)
+        {
+            // Initialize weights randomly between -1 and 1
+            weights = new double[numInputs];
+            var rnd = new Random();
+            for (int i = 0; i < numInputs; i++)
+            {
+                weights[i] = rnd.NextDouble() * 2 - 1;
+            }
+
+            // Initialize bias to 0
+            bias = 0;
+
+            // Set learning rate
+            this.learningRate = learningRate;
+        }
+
+        public int Predict(double[] inputs)
+        {
+            double weightedSum = 0;
+            for (int i = 0; i < weights.Length; i++)
+            {
+                weightedSum += weights[i] * inputs[i];
+            }
+            weightedSum += bias;
+            return weightedSum >= 0 ? 1 : 0;
+        }
+
+        public void Train(double[][] inputs, int[] targets, int numEpochs)
+        {
+            for (int epoch = 0; epoch < numEpochs; epoch++)
+            {
+                for (int i = 0; i < inputs.Length; i++)
+                {
+                    double prediction = Predict(inputs[i]);
+                    double error = targets[i] - prediction;
+                    for (int j = 0; j < weights.Length; j++)
+                    {
+                        weights[j] += learningRate * error * inputs[i][j];
+                    }
+                    bias += learningRate * error;
+                }
+            }
+        }
+    }
+}
